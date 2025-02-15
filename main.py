53bba00c87854bf774961a17c534bd16ae97c90d
Arihan Sharma <sofwarearihan@gmail.com>
2025-02-14 23:43:38 -0800

    mobius api set up

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from mobius import Mobile
 
 app = FastAPI()
 
+mobile = Mobile()
+
 @app.get("/")
 async def root():
+    mobile.do("mobile made on server")
     return {"message": "Hello World"}
---
+++ mobius/mobile.py
@@ -0,0 +1,6 @@
+class Mobile:
+    def __init__(self):
+        print("Mobile initialized!")
+    
+    def do(self, action: str):
+        print("action: " + action)
